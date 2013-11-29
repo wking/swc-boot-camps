646cf7eec04c584977fab0766298c0cbdd0f09f3
Mike Jackson <mikezonk@gmail.com>
2013-11-29 11:05:52 +0000

    Added patient-average.py from instructors

---
+++ patients/patient-average.py
@@ -0,0 +1,11 @@
+total = 0.0
+num = 0
+for p in range(60):
+    if patients[p, 1] == 0:
+        max_count = 0
+        for t in range(40):
+            if patients[p, t] > max_count:
+                max_count = patients[p, t]
+        total += max_count
+        num += 1
+print 'result', total / num
